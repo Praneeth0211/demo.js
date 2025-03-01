f564d5e4adaeee6a6b88bf0cf9a75f250a56228e
saipraneeth ireddy <saipraneeth.ireddy@gmail.com>
2025-03-01 12:23:21 +0530

    my name ali

--- test1.js
+++ test1.js
@@ -4,13 +4,4 @@ console.log(pwd)
 }
 details("sai",1234)
 
-function fn1(){
-    let x = 10;
-    function nfm(){
-        let y = 20
-        console.log(y);
-        console.log(x);
-        
-    }
-}
-    nfm();
+
