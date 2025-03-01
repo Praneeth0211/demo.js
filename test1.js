5b54b208483e638b1a6d6eb09844ab61a35e5209
saipraneeth ireddy <saipraneeth.ireddy@gmail.com>
2025-03-01 11:23:35 +0530

    second message

--- test1.js
+++ test1.js
@@ -3,3 +3,14 @@ console.log(name)
 console.log(pwd)
 }
 details("sai",1234)
+
+function fn1(){
+    let x = 10;
+    function nfm(){
+        let y = 20
+        console.log(y);
+        console.log(x);
+        
+    }
+}
+    nfm();
